691ebc22a02254e0c6bedf924854c200149a6056
Yuanjun Zhong <zhuimengzyj@gmail.com>
2022-03-12 13:36:22 +0800

    update ch06

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "./chapter/e_03.h"
 #include "chapter/e_04.h"
 #include "chapter/e_05.h"
+#include "chapter/e_06.h"
 
 
 #define DD 6.f/2
@@ -10,7 +11,10 @@
 int main() {
     // run_e_03();
     // run_e_04();
-    run_e_05();
+    // run_e_05();
+    run_e_06();
     return 0;
+
+
 }
 
