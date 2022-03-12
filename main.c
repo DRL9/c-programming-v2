676e7bc6218f4db357158a969b279f8b36982bea
Yuanjun Zhong <zhuimengzyj@gmail.com>
2022-03-12 11:38:09 +0800

    update ch04

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-#include "./chapter_03/e_03.h"
+#include <stdbool.h>
+#include "./chapter/e_03.h"
+#include "chapter/e_04.h"
 
 
 #define DD 6.f/2
 
 int main() {
-    run_e_03();
+    // run_e_03();
+    // run_e_04();
+    float a = false ? 1.2f : 3;
+    printf("%f", a);
     return 0;
 }
 
