b26dff7f991d01bb0e6dc62b88bb5ffbdc655f01
Yuanjun Zhong <zhuimengzyj@gmail.com>
2022-03-12 11:51:02 +0800

    update ch05

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include "./chapter/e_03.h"
 #include "chapter/e_04.h"
+#include "chapter/e_05.h"
 
 
 #define DD 6.f/2
@@ -9,8 +10,7 @@
 int main() {
     // run_e_03();
     // run_e_04();
-    float a = false ? 1.2f : 3;
-    printf("%f", a);
+    run_e_05();
     return 0;
 }
 
