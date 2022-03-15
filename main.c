0425bb52be45dc71b4029c7579550ecc5105626c
Yuanjun Zhong <zhuimengzyj@gmail.com>
2022-03-15 21:26:32 +0800

    update ch10

--- main.c
+++ main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include "./chapter/e_03.h"
 #include "chapter/e_04.h"
 #include "chapter/e_05.h"
 #include "chapter/e_06.h"
+#include "chapter/e_10.h"
 
-
-#define DD 6.f/2
 
 int main() {
     // run_e_03();
     // run_e_04();
     // run_e_05();
-    run_e_06();
+    // run_e_06();
+    run_e_10();
     return 0;
-
-
 }
+
 
