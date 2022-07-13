c45b3fe34e1f174724b385a2aa168e55b7ac6466
Yuanjun Zhong <937676516@qq.com>
2022-07-13 08:36:29 +0800

    add chapter 15

--- chapter/e_14.h
+++ chapter/e_14.h
@@ -2,7 +2,7 @@
 //
 
 #ifndef C_PROGRAMMING_V2_E_14_H
-#define C_PROGRAMMING_V2_E_14_H
+#define C_PROGRAMMING_V2_E_14_H int
 
 #endif //C_PROGRAMMING_V2_E_14_H
 
--- main.c
+++ main.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
 #include "./chapter/e_03.h"
 #include "chapter/e_04.h"
 #include "chapter/e_05.h"
@@ -8,20 +5,21 @@
 #include "chapter/e_10.h"
 #include "chapter/e_13.h"
 #include "chapter/e_14.h"
+#include "chapter/e_15.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-
-#define HELLO  "12"  // 注释
+#define HELLO "12" // 注释
 
 int main() {
-    // run_e_03();
-    // run_e_04();
-    // run_e_05();
-    // run_e_06();
-    // run_e_10();
-    // run_13();
-    run_e14();
-
-    return 0;
+  // run_e_03();
+  // run_e_04();
+  // run_e_05();
+  // run_e_06();
+  // run_e_10();
+  // run_13();
+  // run_e14();
+  e_15();
+  return 0;
 }
-
-
